46cf41abeee2bf3fe1383eeaf700634d5d37bb3d
bekzodnj <bekzodjon.norkuziev@gmail.com>
2019-10-04 23:29:29 +0200

    Token, Login & Auth finished

--- middleware/auth.js
+++ middleware/auth.js
@@ -9,13 +9,13 @@ module.exports = function(req, res, next){
     // check token
     if(!token){
         return res.status(401).json({ msg: 'No token, authorization denied'});
-    }
+    } 
 
     //verify token
     try {
         const decoded = jwt.verify(token, config.get('jwtSecret'));
 
-        req.user = decoded;
+        req.user = decoded.user;
         next(); 
     } catch (err) {
         res.status(401).json({ msg: 'Token is not valid' });
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
-
+const bcrypt = require('bcryptjs');
+const { check, validationResult } = require('express-validator');
+const config = require('config');
+const jwt = require('jsonwebtoken');
 const User = require('../../models/User');
 
 // @route   GET api/auth
@@ -10,12 +13,69 @@ const User = require('../../models/User');
 router.get('/', auth, async (req, res)=> {
 
     try {
-        const user = await User.findById(req.user.id);
+        const user = await User.findById(req.user.id).select('-password');
         res.json(user);
     } catch (err) {
         console.log(err.message);
-        res.status(401).send('Server error');
+        res.status(500).send('Server error');
     }
+});
+
+// @route   POST api/auth
+// @desc    Login user
+// @access  Public
+router.post('/', [
+    check('email', 'Please enter a valid email').isEmail(),
+    check('password', 'Password is required').exists()
+], async (req, res)=> {
+    const errors  = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array() });
+    }
+    
+    const { email, password } = req.body;
+
+    try {
+        // check if user/email exists
+        let user = await User.findOne({ email });
+
+        if(!user){
+            return res.status(400).json( {errors: [{msg: 'Invalid credentials' }]});
+        }
+
+        // comparing
+        const isMatch = await bcrypt.compare(password, user.password);
+        
+        if(!isMatch){
+            return res.status(400).json( {errors: [{msg: 'Invalid credentials' }]});
+        }
+        
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+
+        jwt.sign(
+            payload,
+            config.get('jwtSecret'),
+            {
+                expiresIn: 360000
+            },
+            (err, token)=>{
+                if(err) throw err;
+
+                res.json({ token });
+            }
+            )
+
+            
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server error');
+    }
+
+
 });
 
 module.exports = router;
