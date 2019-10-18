46f6e70674928a2734c528b1a2fb4c880a82f230
bekzodnj <bekzodjon.norkuziev@gmail.com>
2019-10-18 17:46:19 +0200

    Added Profile token check

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,28 @@
 const express = require('express');
+const auth = require('../../middleware/auth');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 const router = express.Router();
 
-// @route   GET api/profle
-// @desc    Test Route
-// @access  Public
-router.get('/', (req, res)=> res.send('Profile route'));
+
+// @route   GET api/profle/me
+// @desc    Get current users profile
+// @access  Priate
+router.get('/me', auth, async (req, res)=> {
+    try {
+        const profile = await Profile.findOne({ user: req.user.id }).populate('user', 
+        ['name', 'avatar']);
+
+        if(!profile){
+            return res.status(400).json({ msg: 'There is no profile for this user'});
+        }
+
+        res.json(profile);
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+});
 
 module.exports = router;
