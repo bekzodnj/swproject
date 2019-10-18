049d1246e6041e0f66c3eea48bf4111bbf227f28
bekzodnj <bekzodjon.norkuziev@gmail.com>
2019-10-18 22:24:33 +0200

    Fixed dublicate error in Mongo db

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,7 +3,7 @@ const auth = require('../../middleware/auth');
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
 const router = express.Router();
-const { check, validationResult } = require('express-validator/check');
+const { check, validationResult } = require('express-validator');
 
 // @route   GET api/profle/me
 // @desc    Get current users profile
@@ -37,6 +37,69 @@ router.post('/', [auth, [
     if(!errors.isEmpty()){
         return res.status(400).json({errors: errors.array() }); 
     }
+
+    const {
+        company,
+        website,
+        location,
+        bio,
+        status,
+        githubusername,
+        skills,
+        youtube,
+        facebook,
+        twitter,
+        instagram,
+        linkedin
+    } = req.body;
+
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    
+    if(company) profileFields.company = company;
+    if(website) profileFields.website = website;
+    if(location) profileFields.location = location;
+    if(bio) profileFields.bio = bio;
+    if(status) profileFields.status = status;
+    if(githubusername) profileFields.githubusername = githubusername;
+
+    if(skills){
+        profileFields.skills = skills.split(',').map(el=> el.trim());
+    }
+
+    profileFields.social = {};
+    if(youtube) profileFields.social.youtube = youtube;
+    if(facebook) profileFields.social.facebook = facebook;
+    if(twitter) profileFields.social.twitter = twitter;
+    if(linkedin) profileFields.social.linkedin = linkedin;
+    if(instagram) profileFields.social.instagram = instagram;
+
+    try {
+        let profile = await Profile.findOne({ user: req.user.id });
+
+        //if exists update
+        if(profile){
+            profile = await Profile.findOneAndUpdate(
+                { user: req.user.id },
+                { $set: profileFields},
+                { new: true }
+            );
+
+            return res.json(profile);
+        }
+
+        // create a new profile
+        profile = new Profile(profileFields);
+
+        await profile.save();
+        res.json(profile);
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+
+
 });
 
 module.exports = router;
