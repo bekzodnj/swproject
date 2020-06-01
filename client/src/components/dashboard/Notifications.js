4756eeda67f812427ee85e00fe784ea3a67d9c90
bekzodnj <bekzodnx@gmail.com>
2020-06-01 11:54:09 +0100

    Added Notifiction date

--- client/src/components/dashboard/Notifications.js
+++ client/src/components/dashboard/Notifications.js
@@ -1,53 +1,83 @@
-import React, { Fragment, useEffect } from "react";
-import { Link } from "react-router-dom";
+import React, { Fragment, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
-import { connect } from "react-redux";
+import { connect } from 'react-redux';
 
 import {
   getEnrolledTeacher,
   getEnrolledApprove,
-  getEnrolledReject
-} from "../../actions/services";
+  getEnrolledReject,
+} from '../../actions/services';
 
 export const Notifications = ({
   enrolled,
   getEnrolledTeacher,
   getEnrolledApprove,
-  getEnrolledReject
+  getEnrolledReject,
 }) => {
   useEffect(() => {
     getEnrolledTeacher();
   }, [getEnrolledTeacher]);
 
+  const req_date = {
+    day: '',
+    startHour: '',
+    startMin: '',
+    endHour: '',
+    endMin: '',
+  };
+  if (enrolled !== undefined && enrolled.length > 0) {
+    console.log(enrolled[0].requestedDates);
+
+    const start = new Date(enrolled[0].requestedDates.start);
+    const end = new Date(enrolled[0].requestedDates.end);
+    const dayName = start.toString().split(' ')[0];
+
+    const start_hours = start.getHours();
+    const start_mins = start.getMinutes();
+
+    const end_hours = end.getHours();
+    const end_mins = end.getMinutes();
+
+    req_date.day = dayName;
+    req_date.startHour = start_hours;
+    req_date.startMin = start_mins;
+    req_date.endHour = end_hours;
+    req_date.endMin = end_mins;
+    console.log(req_date);
+  }
+
   //collect from enroll list who is not approved, nor deleted
   let enrolls;
   if (enrolled.length !== 0 && enrolled !== null) {
-    enrolls = enrolled.filter(el => el.is_approved !== true);
+    enrolls = enrolled.filter((el) => el.is_approved !== true);
   } else {
     enrolls = [];
   }
 
-  const acceptHandle = id => {
+  const acceptHandle = (id) => {
     getEnrolledApprove(id);
   };
 
-  const rejectHandle = id => {
+  const rejectHandle = (id) => {
     getEnrolledReject(id);
   };
 
   return (
     <section>
-      <div className="d-flex justify-content-between ">
+      <div className='d-flex justify-content-between '>
         <h2>My Notifications</h2>
       </div>
 
-      <table className="table table-striped mt-3">
+      <table className='table table-striped mt-3'>
         <thead>
           <tr>
-            <th scope="col">Title</th>
-            <th scope="col">Subject</th>
-            <th scope="col">Student ID:</th>
-            <th scope="col">Action</th>
+            <th scope='col'>No</th>
+            <th scope='col'>Title</th>
+            <th scope='col'>Subject</th>
+            <th scope='col'>Student ID:</th>
+            <th scope='col'>Requested Meeting Time</th>
+            <th scope='col'>Action</th>
           </tr>
         </thead>
         {enrolls === undefined ? (
@@ -56,22 +86,35 @@ export const Notifications = ({
           <tbody>
             {enrolls.length !== 0 &&
               enrolls !== undefined &&
-              enrolls.map(el => (
+              enrolls.map((el, idx) => (
                 <tr key={el._id}>
+                  <td>{idx + 1}</td>
                   <td>{el.service.title}</td>
                   <td>{el.service.subject}</td>
                   <td>
                     <Link to={`/student/${el.student}`}>John Doe</Link>
                   </td>
                   <td>
+                    <p>
+                      {req_date.day},{' '}
+                      {req_date.startHour +
+                        ':' +
+                        req_date.startMin +
+                        '-' +
+                        req_date.endHour +
+                        ':' +
+                        req_date.endMin}
+                    </p>
+                  </td>
+                  <td>
                     <button
-                      className="btn btn-primary mr-2"
+                      className='btn btn-primary mr-2'
                       onClick={() => acceptHandle(`${el._id}`)}
                     >
                       Accept
                     </button>
                     <button
-                      className="btn btn-danger mr-2"
+                      className='btn btn-danger mr-2'
                       onClick={() => rejectHandle(`${el._id}`)}
                     >
                       Reject
@@ -86,12 +129,12 @@ export const Notifications = ({
   );
 };
 
-const mapStateToProps = state => ({
-  enrolled: state.enrolled
+const mapStateToProps = (state) => ({
+  enrolled: state.enrolled,
 });
 
 export default connect(mapStateToProps, {
   getEnrolledTeacher,
   getEnrolledApprove,
-  getEnrolledReject
+  getEnrolledReject,
 })(Notifications);
